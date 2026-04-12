45d111a117aa5682d61afb271a86c3a600a6349e
Alyth <134451888+Andrew5331@users.noreply.github.com>
2026-04-12 14:02:09 -0500

    Remove unnecessary blank lines in authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import { login } from '../controllers/authController.js';
-
 const router = express.Router();
-
-router.post('/', login); // POST /login
-
+router.post('/', login); 
 export default router;
